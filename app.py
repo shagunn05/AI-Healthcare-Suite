ad5f95a8fbd14398928b394aafefdc79738411aa
shagunn05 <shagunsharma9376@gmail.com>
2026-06-14 14:44:02 +0530

    app file

--- app.py
+++ app.py
@@ -1,19 +1,4 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="AI Healthcare Suite",
-    page_icon="🧠",
-    layout="wide"
-)
-
-st.title("🧠 AI Healthcare Suite")
-
-st.markdown("""
-### Welcome 👋  
-This system includes:
-- ANN for Diabetes Prediction  
-- CNN for Pneumonia Detection  
-- LSTM/GRU for Time Series Analysis  
-""")
-
-st.sidebar.success("Select a module from pages 👈")
+st.title("App Working Test")
+st.write("If you see this, Streamlit is OK")
