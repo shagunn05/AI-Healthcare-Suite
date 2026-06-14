ca6ec1f8401f938ce960e694b3c4189dd9659870
shagunn05 <shagunsharma9376@gmail.com>
2026-06-14 14:56:40 +0530

    update file

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import numpy as np
+from tensorflow.keras.models import load_model
 
-st.title("App Working Test")
-st.write("If you see this, Streamlit is OK")
+st.title("ANN Test Page")
+
+model = load_model("Models/ann_model.keras")
+
+glucose = st.number_input("Glucose")
+bp = st.number_input("Blood Pressure")
+bmi = st.number_input("BMI")
+
+if st.button("Predict"):
+    data = np.array([[glucose, bp, bmi]])
+    pred = model.predict(data)
+    st.success(pred)
